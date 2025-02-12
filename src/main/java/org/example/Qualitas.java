e50b5646bae601d52de6bd2cb32dae48378b22ac
gonzalez.camar <gonzalezcarrasco1995@gmail.com>
2025-02-12 20:54:04 +0100

    continue with DAO

--- src/main/java/org/example/Qualitas.java
+++ src/main/java/org/example/Qualitas.java
@@ -1,6 +1,84 @@
 package org.example;
 
-public class Qualitas {
+import org.example.Entity.Alumno;
 
-    int numero1 = 0;
+import java.util.Scanner;
+
+public class Qualitas {
+    Scanner sc = new Scanner(System.in);
+    //Métod0 para el registro de usuario
+
+
+
+
+    public void registrarAlumno(){
+        System.out.println("Bienvenido al perfil de alumno. Por favor ingrese los siguientes datos");
+        System.out.println("------------------------------------------------------------------------");
+        String DNI = sc.nextLine();
+        System.out.println("DNI: ");
+        String nombre = sc.nextLine();
+        System.out.println("Nombre: ");
+        String email = sc.nextLine();
+        System.out.println("Email: ");;
+        String fechaNacimiento = sc.nextLine();
+        System.out.println("Fecha de nacimiento: ");
+        String direccion = sc.nextLine();
+        System.out.println("Dirección: ");
+        String telefono= sc.nextLine();
+        System.out.println("Teléfono: ");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("¡INFORMACIÓN AÑADIDA CON ÉXITO!");
+
+
+
+
+
+
+    }
+
+
+    public void registrarTuto(){
+        System.out.println("Bienvenido al perfil de tutor. Por favor ingrese los siguientes datos");
+        System.out.println("------------------------------------------------------------------------");
+        String DNI = sc.nextLine();
+        System.out.println("DNI: ");
+        String nombre= sc.nextLine();
+        System.out.println("Nombre: ");
+        String fechaNacimiento = sc.nextLine();
+        System.out.println("Fecha de nacimiento: ");
+        String direccion = sc.nextLine();
+        System.out.println("Dirección: ");
+        String telefono= sc.nextLine();
+        System.out.println("Teléfono: ");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("¡INFORMACIÓN AÑADIDA CON ÉXITO!");
+
+
+
+    }
+
+
+    public void registrarProfesor(){
+        System.out.println("Bienvenido al perfil de profesor. Por favor ingrese los siguientes datos");
+        System.out.println("------------------------------------------------------------------------");
+        String DNI = sc.nextLine();
+        System.out.println("DNI: ");
+        String nombre= sc.nextLine();
+        String email = sc.nextLine();
+        System.out.println("Email: ");;
+        System.out.println("Nombre: ");
+        String fechaNacimiento = sc.nextLine();
+        System.out.println("Fecha de nacimiento: ");
+        String telefono = sc.nextLine();
+        System.out.println("Teléfono: ");
+        System.out.println("-----------------------------------------------------------------------");
+        System.out.println("¡INFORMACIÓN AÑADIDA CON ÉXITO!");
+
+
+
+
+
+
+
+    }
 }
